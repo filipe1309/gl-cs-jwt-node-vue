858a7fdac5174e3b695abf4f54216b4cfcb1f62f
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-12 19:45:13 -0300

    chore: add db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const db = require('./config/db.config');
 
 const app = express();
+
+// DB Connection
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(db.local.localDatabaseUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(
+        () => {
+            console.log('DB connection successed');
+        },
+        (err) => {
+            console.log(`DB connection failed: ${err}`);
+            process.exit();
+        }
+    );
 
 // Routes
 const index = require('./routes/index');
