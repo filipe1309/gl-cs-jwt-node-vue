8817ce37fba9cdd9c0be05f6aae5c949ad36ee64
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-06 01:51:18 -0300

    feat: add login user endpoint

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controllers');
 
-// Route to create new 'User': POST http://localhot:3000/api/v1/register
+// Route to create new 'User': POST http://localhot:8080/api/v1/register
 router.post('/register', userController.registerNewUser);
 
+// Route to login a 'User': POST http://localhot:8080/api/v1/login
 router.post('/login', userController.loginUser);
 
+// Route to get the profile of a 'User': GET http://localhot:8080/api/v1/user-profile
 router.get('/user-profile', userController.returnUserProfile);
 
 module.exports = router;
