a7e0192c02d772ae0eabbb75d612a0859ae22d89
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-06 00:42:44 -0300

    refactor: separate moongose connection from app main file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
-const db = require('./config/db.config');
 
 const app = express();
-
-// DB Connection
-mongoose.Promise = global.Promise;
-mongoose
-    .connect(db.local.localDatabaseUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    })
-    .then(
-        () => {
-            console.log('DB connection successed');
-        },
-        (err) => {
-            console.log(`DB connection failed: ${err}`);
-            process.exit();
-        }
-    );
 
 // Routes
 const index = require('./routes/index');
@@ -35,6 +15,7 @@ app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 app.use(morgan('dev'));
 app.use(index);
-// TODO: add user.routes.js route
+const mongooseConnection = require('./config/mongooseConnection.config');
+app.set('mongoose connection', mongooseConnection);
 
 module.exports = app;
--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv');
 
+dotenv.config();
+
 module.exports = {
     local: {
         localDatabaseUrl: process.env.DB_URI,
